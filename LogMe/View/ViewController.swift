02909b7fe62a9d1407a59b49abdf51ac888c74ce
howardC56 <h2chang56@gmail.com>
2019-09-25 14:45:31 -0400

    Made ViewControllers

---
+++ LogMe/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LogMe
+//
+//  Created by Howard Chang on 9/24/19.
+//  Copyright © 2019 Howard Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
